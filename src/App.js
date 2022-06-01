5bc0d749305888ce1f6c7d7d0971514b1b6a00ad
SarahRex666 <95315055+SarahRex666@users.noreply.github.com>
2022-06-01 11:50:37 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import "./App.css";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Card from "./Card";
 
 function App() {
   const [search, setSearch] = useState("");
+  const [zodiacData, setZodiacData] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3000/cards/")
+      .then((r) => r.json())
+      .then((data) => setZodiacData(data));
+  }, []);
 
   function inputHandler(event) {
     setSearch(event.target.value);
@@ -18,7 +25,7 @@ function App() {
 
   return (
     <div className="App">
-      <Card search={search} setSearch={setSearch} />
+      <Card zodiacData={zodiacData} search={search} setSearch={setSearch} />
       <form onSubmit={submitHandler}>
         <input
           placeholder="Enter Your Zodiac Sign"
